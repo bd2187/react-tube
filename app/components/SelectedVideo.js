5cb1555ca5139b28c5419259ff94790713d69a3a
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2017-06-28 15:54:06 -0700

    Change SelectedVideo component from class component to stateleless functional component.

--- app/components/SelectedVideo.js
+++ app/components/SelectedVideo.js
@@ -3,32 +3,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-class SelectedVideo extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    var selectedVideo = this.props.selectedVideo;
-    return (
-      <div className="selected-video">
-        <iframe
-          allowFullScreen
-          src={`https://www.youtube.com/embed/${selectedVideo.id.videoId}`}>
-        </iframe>
-        <div className="video-description">
-          <h3>{selectedVideo.snippet.title}</h3>
-          <p>Published on {selectedVideo.snippet.publishedAt}</p>
-          <p>{selectedVideo.snippet.description}</p>
-        </div>
+function SelectedVideo(props) {
+  var selectedVideo = props.selectedVideo;
+  return (
+    <div className="selected-video">
+      <iframe
+        allowFullScreen
+        src={`https://www.youtube.com/embed/${selectedVideo.id.videoId}`}>
+      </iframe>
+      <div className="video-description">
+        <h3>{selectedVideo.snippet.title}</h3>
+        <p>Published on {selectedVideo.snippet.publishedAt}</p>
+        <p>{selectedVideo.snippet.description}</p>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 SelectedVideo.propTypes = {
   selectedVideo: PropTypes.object.isRequired,
   changeSelectedVideo: PropTypes.func.isRequired
-
 }
 
 export default SelectedVideo;
