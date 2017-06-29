d03d93b71c7ac057b368a8be5eaf44f7f647681f
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2017-06-28 18:26:30 -0700

    Adjust contents of SearchBar component

--- app/components/SearchBar.js
+++ app/components/SearchBar.js
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
+import Logo from '../utils/img/youtube.png';
 
 class SearchBar extends React.Component {
   constructor(props) {
@@ -25,21 +26,25 @@ class SearchBar extends React.Component {
   render() {
     var searchValue = this.state.searchValue;
     return (
-      <div className="search-bar">
-      <img className="logo"
-        src={require('../youtube.png')} alt="youtube logo"/>
-        <form onSubmit={this.handleSubmit}>
-          <input
-            type="text"
-            value={searchValue}
-            placeholder="Search"
-            autoComplete="off"
-            onChange={this.handleInput}/>
-          <button
-            disabled={!searchValue}>
-              <i className="fa fa-search" aria-hidden="true"></i>
-          </button>
-        </form>
+      <div className="search-bar clearfix">
+        <a href="#" className="logo-link">
+          <img className="logo"
+            src={Logo} alt="youtube logo"/>
+        </a>
+        <div className="form-box">
+          <form onSubmit={this.handleSubmit}>
+            <input
+              type="text"
+              value={searchValue}
+              placeholder="Search"
+              autoComplete="off"
+              onChange={this.handleInput}/>
+            <button
+              disabled={!searchValue}>
+                <i className="fa fa-search" aria-hidden="true"></i>
+            </button>
+          </form>
+        </div>
       </div>
     )
   }
