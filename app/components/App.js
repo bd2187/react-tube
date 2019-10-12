838a065986dd1f0e49f5150f91b0874130401e79
bd2187 <brandon.dionisio.26@gmail.com>
2019-10-12 11:01:39 -0700

    Import and render routes

--- app/components/App.js
+++ app/components/App.js
@@ -1,55 +1,9 @@
-'use strict';
+"use strict";
 
-import React from 'react';
-import SearchBar from './SearchBar';
-import SelectedVideo from './SelectedVideo';
-import Videos from './Videos';
-import YoutubeSearch from '../utils/api';
+import routes from "../config/routes";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      searchTerm: 'React JS',
-      videosArr: null,
-      selectedVideo: null
-    };
-    this.handleSearch = this.handleSearch.bind(this);
-    this.changeSelectedVideo = this.changeSelectedVideo.bind(this);
-  }
-  handleSearch(searchTerm) {
-    YoutubeSearch(searchTerm, (videosArr = []) => {
-      this.setState({ videosArr, selectedVideo: videosArr[0] });
-    });
-  }
-  changeSelectedVideo(video) {
-    YoutubeSearch(video.snippet.title, (videosArr = []) => {
-      this.setState({ videosArr });
-    });
+const App = () => {
+    return routes;
+};
 
-    this.setState(function() {
-      return { selectedVideo: video };
-    });
-  }
-  componentDidMount() {
-    this.handleSearch(this.state.searchTerm);
-  }
-  render() {
-    var videosArr = this.state.videosArr;
-    return (
-      <div className="container">
-        <SearchBar onSubmit={this.handleSearch} />
-        {!videosArr
-          ? <div className="loader" />
-          : <div className="main-box clearfix">
-              <SelectedVideo selectedVideo={this.state.selectedVideo} />
-              <Videos
-                videosArr={videosArr}
-                changeSelectedVideo={this.changeSelectedVideo}
-              />
-            </div>}
-      </div>
-    );
-  }
-}
 export default App;
