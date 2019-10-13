e233e9751cd68d93ff8d497f6e15c905f015b804
bd2187 <brandon.dionisio.26@gmail.com>
2019-10-13 11:53:16 -0700

    Pass store.searchedVideos to VideoContainer as props

--- app/containers/VideoContainer.js
+++ app/containers/VideoContainer.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { connect } from "react-redux";
 
-const VideoContainer = () => {
+const VideoContainer = props => {
     return <h1>VideoContianer</h1>;
 };
 
-export default VideoContainer;
+var mapStateToProps = function(store) {
+    return {
+        searchedVideos: store.searchedVideos
+    };
+};
+
+export default connect(
+    mapStateToProps,
+    null
+)(VideoContainer);
