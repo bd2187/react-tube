b12d6111e8c51890fb4bdb28e39850719803d120
bd2187 <brandon.dionisio.26@gmail.com>
2019-10-13 11:56:42 -0700

    Pass redux store to App component. Apply thunk as middleware for store

--- app/index.js
+++ app/index.js
@@ -1,13 +1,27 @@
 "use strict";
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-import NormalizeCss from './styles/css/normalize.css';
-import MainCss from './styles/css/main.css';
+import React from "react";
+import { render } from "react-dom";
+import App from "./components/App";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import reducers from "./reducers/reducers";
+import NormalizeCss from "./styles/css/normalize.css";
+import MainCss from "./styles/css/main.css";
 
+const store = createStore(
+    reducers,
+    compose(
+        applyMiddleware(thunk),
+        window.__REDUX_DEVTOOLS_EXTENSION__ &&
+            window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+);
 
-ReactDOM.render(
-  <App/>,
-  document.getElementById('app')
+render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById("app")
 );
