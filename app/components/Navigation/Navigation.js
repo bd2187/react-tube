b2a4fe811769cef2d516e1146d6fb4eeaf7d89f9
bd2187 <brandon.dionisio.26@gmail.com>
2019-10-14 21:15:36 -0700

    Add searchBarOpened and navMenuOpened portions of state. Add logic which toggle the NavigationMenu and the SearchBar

--- app/components/Navigation/Navigation.js
+++ app/components/Navigation/Navigation.js
@@ -1,9 +1,19 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
 import Link from "../Link";
+import styles from "./Navigation.module.css";
+import SearchBar from "./SearchBar";
+import NavigationMenu from "./NavigationMenu";
 
-const Navigation = function Navigation({ updateVideos }) {
+/**
+ * Renders the Navigation header.
+ * This also handles toggling both the search-bar
+ * and navigation menu.
+ */
+const Navigation = function Navigation({ updateVideos, user }) {
     const [userQuery, setQuery] = useState("");
+    const [searchBarOpened, setSearchBarOpened] = useState(false);
+    const [navMenuOpened, setNavMenuOpened] = useState(false);
 
     const onSubmit = function onSubmit(evt) {
         evt.preventDefault();
@@ -15,88 +25,68 @@ const Navigation = function Navigation({ updateVideos }) {
         setQuery(value);
     };
 
+    const toggleSearchBar = function toggleSearchBar() {
+        setSearchBarOpened(!searchBarOpened);
+        var searchInput = document.getElementsByClassName(
+            `${styles["search-form-container__text-input"]}`
+        )[0];
+
+        if (!searchBarOpened) {
+            setTimeout(() => {
+                searchInput.focus();
+            }, 0);
+        } else {
+            setTimeout(() => {
+                searchInput.blur();
+            }, 0);
+        }
+    };
+
+    const toggleNavMenu = function toggleNavMenu() {
+        setNavMenuOpened(!navMenuOpened);
+    };
+
     return (
-        <div className="search-bar clearfix">
-            {/* <Link
-                            resetState={this.props.onSubmit}
-                            clearInputField={this.clearInputField}
-                        /> */}
-            <div className="form-box">
-                <form onSubmit={onSubmit}>
-                    <input
-                        type="text"
-                        value={userQuery}
-                        placeholder="Search"
-                        autoComplete="off"
-                        onChange={updateValue}
-                    />
-                    <button disabled={!userQuery}>
-                        <i className="fa fa-search" aria-hidden="true"></i>
-                    </button>
-                </form>
+        <div
+            className={`${styles["header"]} ${
+                searchBarOpened ? styles["header--open-search-bar"] : ""
+            }`}
+        >
+            {/* <Link /> */}
+            <div className={`${styles["header_logo"]}`}>
+                <h1>logo</h1>
             </div>
+
+            <SearchBar
+                onSubmit={onSubmit}
+                userQuery={userQuery}
+                updateValue={updateValue}
+                toggleSearchBar={toggleSearchBar}
+            />
+
+            <div className={`${styles["header_icons-container"]}`}>
+                <i
+                    onClick={toggleSearchBar}
+                    className="fa fa-search"
+                    aria-hidden="true"
+                ></i>
+                <i onClick={toggleNavMenu} className="fa fa-user-circle"></i>
+            </div>
+
+            {navMenuOpened ? (
+                <NavigationMenu toggleNavMenu={toggleNavMenu} user={user} />
+            ) : null}
         </div>
     );
 };
 
-// class Navigation extends React.Component {
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//             searchValue: ""
-//         };
-//         this.handleInput = this.handleInput.bind(this);
-//         this.handleSubmit = this.handleSubmit.bind(this);
-//         this.clearInputField = this.clearInputField.bind(this);
-//     }
-//     handleInput(event) {
-//         var searchValue = event.target.value;
-//         this.setState(function() {
-//             return { searchValue };
-//         });
-//     }
-//     handleSubmit(event) {
-//         event.preventDefault();
-//         this.props.onSubmit(this.state.searchValue);
-//     }
-//     clearInputField() {
-//         this.setState({ searchValue: "" });
-//     }
-//     componentDidMount() {
-//         this.textInput.focus();
-//     }
-//     render() {
-//         var searchValue = this.state.searchValue;
-//         return (
-//             <div className="search-bar clearfix">
-//                 <Link
-//                     resetState={this.props.onSubmit}
-//                     clearInputField={this.clearInputField}
-//                 />
-//                 <div className="form-box">
-//                     <form onSubmit={this.handleSubmit}>
-//                         <input
-//                             type="text"
-//                             value={searchValue}
-//                             placeholder="Search"
-//                             autoComplete="off"
-//                             ref={input => {
-//                                 this.textInput = input;
-//                             }}
-//                             onChange={this.handleInput}
-//                         />
-//                         <button disabled={!searchValue}>
-//                             <i className="fa fa-search" aria-hidden="true"></i>
-//                         </button>
-//                     </form>
-//                 </div>
-//             </div>
-//         );
-//     }
-// }
-
 Navigation.propTypes = {
-    updateVideos: PropTypes.func.isRequired
+    updateVideos: PropTypes.func.isRequired,
+    user: PropTypes.shape({
+        email: PropTypes.string.isRequired,
+        username: PropTypes.string.isRequired,
+        id: PropTypes.string.isRequired
+    })
 };
 
 export default Navigation;
