379ec1b31e8641ec595914f5c0c015aaac137603
bd2187 <brandon.dionisio.26@gmail.com>
2019-10-15 21:56:36 -0700

    Format code with prettier

--- app/components/SearchBar.js
+++ app/components/SearchBar.js
@@ -1,62 +1,67 @@
 "use strict";
 
-import React from 'react';
-import PropTypes from 'prop-types';
-import Link from './Link';
+import React from "react";
+import PropTypes from "prop-types";
+import Link from "./Link";
 
 class SearchBar extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      searchValue: ''
+    constructor(props) {
+        super(props);
+        this.state = {
+            searchValue: ""
+        };
+        this.handleInput = this.handleInput.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.clearInputField = this.clearInputField.bind(this);
     }
-    this.handleInput = this.handleInput.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.clearInputField = this.clearInputField.bind(this);
-  }
-  handleInput(event) {
-    var searchValue = event.target.value;
-    this.setState(function(){
-      return {searchValue}
-    });
-  }
-  handleSubmit(event) {
-    event.preventDefault();
-    this.props.onSubmit(this.state.searchValue);
-  }
-  clearInputField() {
-    this.setState({searchValue: ""});
-  }
-  componentDidMount() {
-    this.textInput.focus();
-  }
-  render() {
-    var searchValue = this.state.searchValue;
-    return (
-      <div className="search-bar clearfix">
-        <Link resetState={this.props.onSubmit} clearInputField={this.clearInputField}/>
-        <div className="form-box">
-          <form onSubmit={this.handleSubmit}>
-            <input
-              type="text"
-              value={searchValue}
-              placeholder="Search"
-              autoComplete="off"
-              ref={(input) => {this.textInput = input;}}
-              onChange={this.handleInput}/>
-            <button
-              disabled={!searchValue}>
-                <i className="fa fa-search" aria-hidden="true"></i>
-            </button>
-          </form>
-        </div>
-      </div>
-    );
-  }
+    handleInput(event) {
+        var searchValue = event.target.value;
+        this.setState(function() {
+            return { searchValue };
+        });
+    }
+    handleSubmit(event) {
+        event.preventDefault();
+        this.props.onSubmit(this.state.searchValue);
+    }
+    clearInputField() {
+        this.setState({ searchValue: "" });
+    }
+    componentDidMount() {
+        this.textInput.focus();
+    }
+    render() {
+        var searchValue = this.state.searchValue;
+        return (
+            <div className="search-bar clearfix">
+                <Link
+                    resetState={this.props.onSubmit}
+                    clearInputField={this.clearInputField}
+                />
+                <div className="form-box">
+                    <form onSubmit={this.handleSubmit}>
+                        <input
+                            type="text"
+                            value={searchValue}
+                            placeholder="Search"
+                            autoComplete="off"
+                            ref={input => {
+                                this.textInput = input;
+                            }}
+                            onChange={this.handleInput}
+                        />
+                        <button disabled={!searchValue}>
+                            <i className="fa fa-search" aria-hidden="true"></i>
+                        </button>
+                    </form>
+                </div>
+            </div>
+        );
+    }
 }
 
 SearchBar.propTypes = {
-  onSubmit: PropTypes.func.isRequired
-}
+    onSubmit: PropTypes.func.isRequired
+};
 
 export default SearchBar;
