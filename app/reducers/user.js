cc6f689275dd880333cac89ad316b43c872aedf2
bd2187 <brandon.dionisio.26@gmail.com>
2019-10-15 22:03:15 -0700

    Initial commit

---
+++ app/reducers/user.js
@@ -0,0 +1,13 @@
+const initialState = {
+    id: "",
+    username: "",
+    email: "",
+    favorites: [],
+    token: ""
+};
+
+function user(state = initialState, action) {
+    return state;
+}
+
+export default user;
