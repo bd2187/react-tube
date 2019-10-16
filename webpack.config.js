3ba0d65cc0c0f71d67a8cdc2f2c26f4014441efb
bd2187 <brandon.dionisio.26@gmail.com>
2019-10-15 21:55:36 -0700

    Configure module settings for css modules

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,53 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const webpack = require('webpack');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
 
 var config = {
-  entry: './app/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'index_bundle.js',
-    publicPath: '/'
-  },
-  module: {
-    rules: [
-      { test: /\.(js)$/, use: 'babel-loader' },
-      { test: /\.css$/, use: [ 'style-loader', 'css-loader' ]},
-      { test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192' }
+    devtool: "cheap-module-inline-source-map",
+    entry: "./app/index.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "index_bundle.js",
+        publicPath: "/"
+    },
+    module: {
+        rules: [
+            { test: /\.(js)$/, use: "babel-loader" },
+            //   { test: /\.css$/, use: [ 'style-loader', 'css-loader' ]},
+            {
+                test: /\.css$/,
+                loader: "style-loader"
+            },
+            {
+                test: /\.css$/,
+                loader: "css-loader",
+                query: {
+                    modules: true,
+                    localIdentName: "[name]__[local]___[hash:base64:5]"
+                }
+            },
+            { test: /\.(png|jpg)$/, loader: "url-loader?limit=8192" }
+        ]
+    },
+    devServer: {
+        historyApiFallback: true
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "app/index.html"
+        })
     ]
-  },
-  devServer: {
-    historyApiFallback: true
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'app/index.html'
-    })
-  ]
-}
+};
 
 module.exports = config;
 
-if (process.env.NODE_ENV === 'production') {
-  config.plugins.push(
-    new webpack.DefinePlugin({
-      'process.env': {
-        'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-      }
-    }),
-    new webpack.optimize.UglifyJsPlugin()
-  )
+if (process.env.NODE_ENV === "production") {
+    config.plugins.push(
+        new webpack.DefinePlugin({
+            "process.env": {
+                NODE_ENV: JSON.stringify(process.env.NODE_ENV)
+            }
+        }),
+        new webpack.optimize.UglifyJsPlugin()
+    );
 }
