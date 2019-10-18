bcf63debfdbf39102fec2f8940fe7fb675743724
bd2187 <brandon.dionisio.26@gmail.com>
2019-10-17 22:44:44 -0700

    Initial commit. Create signUpUser() and signInUser()

---
+++ app/actions/userAuthenticationActions.js
@@ -0,0 +1,63 @@
+import axios from "axios";
+import {
+    USER_SIGNED_IN,
+    USER_SIGNED_OUT,
+    USER_SIGNED_UP,
+    USER_AUTHENTICATION_LOADING,
+    USER_AUTHENTICATION_ERROR
+} from "./actionTypes";
+
+const decryptToken = function decryptToken(token) {
+    // decrypt token here
+    return {
+        id: "123abc",
+        username: "123abc",
+        email: "abc@gmail.com",
+        favorites: [],
+        token: "123456hash"
+    };
+};
+
+export function signUpUser({ username, email, password, matchingPassword }) {
+    return function(dispatch) {
+        dispatch({ type: USER_AUTHENTICATION_LOADING });
+
+        return axios
+            .post("http://localhost:5000/user/signup", {
+                username,
+                email,
+                password,
+                matchingPassword
+            })
+            .then(function(res) {
+                const user = decryptToken();
+
+                dispatch({ type: USER_SIGNED_UP, user });
+            })
+            .catch(function(err) {
+                dispatch({ type: USER_AUTHENTICATION_ERROR });
+            });
+    };
+}
+
+export function signInUser({ email, password }) {
+    return function(dispatch) {
+        dispatch({ type: USER_AUTHENTICATION_LOADING });
+
+        return axios
+            .post("http://localhost:5000/user/signin", {
+                email,
+                password
+            })
+            .then(function(res) {
+                const user = decryptToken();
+
+                dispatch({ type: USER_SIGNED_IN, user });
+            })
+            .catch(function(err) {
+                dispatch({ type: USER_AUTHENTICATION_ERROR });
+            });
+    };
+}
+
+function signOutUser({}) {}
