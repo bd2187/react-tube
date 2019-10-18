6a3685b8d2c7c221cb5dec71f3b21bacdf83cee6
bd2187 <brandon.dionisio.26@gmail.com>
2019-10-17 22:41:59 -0700

    Import user action types. Add authError and loadingAuth properties to initialState. Add switch statement for different action types in user action creator

--- app/reducers/user.js
+++ app/reducers/user.js
@@ -1,13 +1,53 @@
+import {
+    USER_SIGNED_IN,
+    USER_SIGNED_OUT,
+    USER_SIGNED_UP,
+    USER_AUTHENTICATION_LOADING,
+    USER_AUTHENTICATION_ERROR
+} from "../actions/actionTypes";
+
 const initialState = {
     id: "",
     username: "",
     email: "",
     favorites: [],
-    token: ""
+    token: "",
+    authError: false,
+    loadingAuth: false
 };
 
 function user(state = initialState, action) {
-    return state;
+    const { user } = action;
+    switch (action.type) {
+        case USER_SIGNED_UP:
+        case USER_SIGNED_IN:
+            return {
+                ...state,
+                authError: false,
+                loadingAuth: false,
+                id: user.id,
+                username: user.username,
+                email: user.email,
+                favorites: user.favorites,
+                token: user.token
+            };
+
+        case USER_AUTHENTICATION_LOADING:
+            return {
+                ...state,
+                authError: false,
+                loadingAuth: true
+            };
+
+        case USER_AUTHENTICATION_ERROR:
+            return {
+                ...state,
+                authError: true,
+                loadingAuth: false
+            };
+        default:
+            return state;
+    }
 }
 
 export default user;
