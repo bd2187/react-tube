b55af1fe3798dbeb147f97b8e85a37e7c1e8b634
bd2187 <brandon.dionisio.26@gmail.com>
2019-10-20 16:55:02 -0700

    Use proptypes. Redirect to / if user is already signed in.

--- app/containers/SignInContainer.js
+++ app/containers/SignInContainer.js
@@ -2,21 +2,28 @@ import React from "react";
 import SignIn from "../components/SignIn/SignIn";
 import { signInUser } from "../actions/userAuthenticationActions";
 import { connect } from "react-redux";
+import { Redirect } from "react-router-dom";
+import PropTypes from "prop-types";
 
-const SignInContainer = function SignInContainer({ signInUser }) {
-    return <SignIn signInUser={signInUser} />;
+const SignInContainer = function SignInContainer({ userID, signInUser }) {
+    return userID ? <Redirect to="/" /> : <SignIn signInUser={signInUser} />;
 };
 
 const mapStateToProps = function(state) {
-    return {};
+    return { userID: state.user.id };
 };
 
 const mapDispatchToProps = function(dispatch) {
     return {
-        signInUser: function({ email, password }) {
+        signInUser: function(email, password) {
             return dispatch(signInUser({ email, password }));
         }
     };
+};
+
+SignInContainer.propTypes = {
+    userID: PropTypes.string.isRequired,
+    signInUser: PropTypes.func.isRequired
 };
 
 export default connect(
