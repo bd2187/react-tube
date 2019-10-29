bddd3e5f1aad0f19efd2024815cc1398094d3712
bd2187 <brandon.dionisio.26@gmail.com>
2019-10-28 21:33:32 -0700

    Get user token from localStorage. Decrypt token and call logUserInfo to update redux store with user info

--- app/index.js
+++ app/index.js
@@ -7,6 +7,8 @@ import { Provider } from "react-redux";
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import reducers from "./reducers/reducers";
+import { decryptToken } from "./config/jwtToken";
+import { logUserInfo } from "./actions/userAuthenticationActions";
 import NormalizeCss from "./styles/css/normalize.css";
 import MainCss from "./styles/css/main.css";
 
@@ -18,6 +20,26 @@ const store = createStore(
             window.__REDUX_DEVTOOLS_EXTENSION__()
     )
 );
+
+var token = localStorage.getItem("token");
+
+if (token) {
+    const user = decryptToken(token);
+
+    store.dispatch(logUserInfo({ ...user, token }));
+
+    // Get current time
+    //   const currentTime = Date.now() / 1000;
+    //   const tokenExp = decoded.exp;
+
+    //   /*
+    //     If the token expired, log the user out by dispatching the logOutUser action.
+    //     Otherwise, log the user in by dispatching logUserInfo(decoded)
+    //   */
+    //   tokenExp < currentTime
+    //     ? store.dispatch(logOutUser())
+    //     : store.dispatch(logUserInfo(decoded));
+}
 
 render(
     <Provider store={store}>
