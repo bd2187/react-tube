6005088bc13969eed95a97292d43c682d0ab9df3
bd2187 <brandon.dionisio.26@gmail.com>
2019-10-30 20:24:52 -0700

    Utilize past-tense when using action types

--- app/actions/searchedVideosActions.js
+++ app/actions/searchedVideosActions.js
@@ -1,19 +1,19 @@
 import API_KEY from "../config/api_key";
 import SearchVideos from "youtube-api-search";
 import {
-    LOADING_VIDEO_QUERY,
-    UPDATE_VIDEOS,
+    LOADED_VIDEO_QUERY,
+    UPDATED_VIDEOS,
     ERROR_FETCHING_VIDEOS
 } from "./actionTypes";
 
 function updateVideos(query) {
     return function(dispatch) {
-        dispatch({ type: LOADING_VIDEO_QUERY, query });
+        dispatch({ type: LOADED_VIDEO_QUERY, query });
 
         return SearchVideos({ key: API_KEY, term: query }, function(data) {
             try {
                 return data && data.length > 0
-                    ? dispatch({ type: UPDATE_VIDEOS, videos: data })
+                    ? dispatch({ type: UPDATED_VIDEOS, videos: data })
                     : dispatch({ type: ERROR_FETCHING_VIDEOS });
             } catch (err) {
                 return dispatch({ type: ERROR_FETCHING_VIDEOS });
