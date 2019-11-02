fb013c8455eab16bd2b58c44d114a5132de2b433
bd2187 <brandon.dionisio.26@gmail.com>
2019-11-02 10:26:45 -0700

    Render VideoWrap component

--- app/components/Video/Video.js
+++ app/components/Video/Video.js
@@ -1,7 +1,45 @@
 import React from "react";
+import SelectedVideo from "./SelectedVideo";
+import VideosList from "./VideosList";
 
-const Video = function Video() {
-    return <h1>Video</h1>;
+const VideoWrap = function VideoWrap({ currentVideo, videos }) {
+    console.log(currentVideo);
+
+    return (
+        <div>
+            <SelectedVideo selectedVideo={currentVideo} />
+            <VideosList videos={videos} />
+        </div>
+    );
+};
+
+const Video = function Video({ searchedVideos }) {
+    const {
+        loadingVideoQuery,
+        videos,
+        error,
+        errorMessage,
+        currentVideo,
+        query
+    } = searchedVideos;
+
+    // if loadingVideoQuery is true, render loading component
+
+    // if there's an error, render the error component,
+
+    // display videos
+
+    return (
+        <>
+            {loadingVideoQuery ? <p>loading</p> : null}
+
+            {error ? <p>error</p> : null}
+
+            {videos.length > 0 ? (
+                <VideoWrap currentVideo={currentVideo} videos={videos} />
+            ) : null}
+        </>
+    );
 };
 
 export default Video;
