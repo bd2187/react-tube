08b613d303ce0a44ce8091bbb3861be9cf78bdeb
bd2187 <brandon.dionisio.26@gmail.com>
2019-11-03 20:35:46 -0800

    Assign classnames from Video.module.css

--- app/components/Video/SelectedVideo.js
+++ app/components/Video/SelectedVideo.js
@@ -1,19 +1,18 @@
-"use strict";
-
 import React from "react";
 import PropTypes from "prop-types";
 import date from "../../utils/date";
+import styles from "./Video.module.css";
 
 function SelectedVideo({ selectedVideo }) {
     return (
-        <div className="selected-video">
-            <div className="aspect-ratio">
+        <div className={styles["selected-video"]}>
+            <div className={styles["selected-video__aspect-ratio"]}>
                 <iframe
                     allowFullScreen
                     src={`https://www.youtube.com/embed/${selectedVideo.id.videoId}`}
                 ></iframe>
             </div>
-            <div className="video-description">
+            <div className={styles["selected-video__description"]}>
                 <h3>{selectedVideo.snippet.title}</h3>
                 <p className="publish-date">
                     Published on {date(selectedVideo.snippet.publishedAt)}
