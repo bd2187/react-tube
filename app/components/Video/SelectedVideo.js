43262586ed4bd1dfbae72a762e0c2a19ade9c429
bd2187 <brandon.dionisio.26@gmail.com>
2019-11-04 21:58:49 -0800

    Add displayInfo state

--- app/components/Video/SelectedVideo.js
+++ app/components/Video/SelectedVideo.js
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import date from "../../utils/date";
 import styles from "./Video.module.css";
 
 function SelectedVideo({ selectedVideo }) {
+    const [displayInfo, setDisplayInfo] = useState(false);
+    const toggleInfo = function() {
+        setDisplayInfo(!displayInfo);
+    };
+
     return (
         <div className={styles["selected-video"]}>
             <div className={styles["selected-video__aspect-ratio"]}>
@@ -12,14 +17,46 @@ function SelectedVideo({ selectedVideo }) {
                     src={`https://www.youtube.com/embed/${selectedVideo.id.videoId}`}
                 ></iframe>
             </div>
-            <div className={styles["selected-video__description"]}>
-                <h3>{selectedVideo.snippet.title}</h3>
-                <p className="publish-date">
-                    Published on {date(selectedVideo.snippet.publishedAt)}
-                </p>
-                <p className="description">
-                    {selectedVideo.snippet.description}
-                </p>
+            <div
+                className={`
+                ${styles["selected-video-info"]}
+                ${
+                    displayInfo
+                        ? styles["selected-video-info--display"]
+                        : styles["selected-video-info--hide"]
+                }
+            `}
+            >
+                <div
+                    className={styles["selected-video-info-header"]}
+                    onClick={toggleInfo}
+                >
+                    <h3 className={styles["selected-video-info-header__title"]}>
+                        {selectedVideo.snippet.title}
+                    </h3>
+                    <i
+                        className={`
+                        ${styles["selected-video-info-header__toggle"]}
+                        fa fa-chevron-down
+                    `}
+                    ></i>
+                </div>
+                <div className={styles["selected-video-info-user-actions"]}>
+                    <i className="fa fa-heart"></i>
+                </div>
+
+                <div className={styles["selected-video-info__description"]}>
+                    <p
+                        className={
+                            styles["selected-video-info-header__publish-date"]
+                        }
+                    >
+                        Published on {date(selectedVideo.snippet.publishedAt)}
+                    </p>
+                    <p className="description">
+                        {selectedVideo.snippet.description}
+                    </p>
+                </div>
             </div>
         </div>
     );
