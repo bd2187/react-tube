e8979a8000f3f7b4c217e0dfbaf8c7e1195a9ae9
bd2187 <brandon.dionisio.26@gmail.com>
2019-11-05 21:40:08 -0800

    Add links to videos

--- app/components/Video/VideosList.js
+++ app/components/Video/VideosList.js
@@ -2,23 +2,30 @@
 
 import React from "react";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 const VideosList = function VideosList({ videos }) {
     return (
         <ul>
             {videos.map(function(video) {
                 return (
-                    <li
-                        key={video.etag}
-                        onClick={() => this.changeDisplayedVideo(index)}
-                        className="video-list-item clearfix"
-                    >
-                        <img
-                            src={video.snippet.thumbnails.high.url}
-                            alt={`${video.snippet.title} video`}
-                            className="video-thumbnail"
-                        />
-                        <h2 className="video-title">{video.snippet.title}</h2>
+                    <li key={video.etag}>
+                        <Link
+                            to={`/${encodeURIComponent(video.snippet.title)}/${
+                                video.id.videoId
+                            }`}
+                        >
+                            <div>
+                                <img
+                                    src={video.snippet.thumbnails.high.url}
+                                    alt={`${video.snippet.title} video`}
+                                    className="video-thumbnail"
+                                />
+                            </div>
+                            <h2 className="video-title">
+                                {video.snippet.title}
+                            </h2>
+                        </Link>
                     </li>
                 );
             })}
