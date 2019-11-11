de8af60c9f2f675564459e4bbf3a28ed9cab8435
bd2187 <brandon.dionisio.26@gmail.com>
2019-11-10 17:30:52 -0800

    Add useEffect hook to watch for videoID

--- app/containers/VideoContainer.js
+++ app/containers/VideoContainer.js
@@ -1,18 +1,23 @@
 import React, { useEffect } from "react";
 import { connect } from "react-redux";
+
 import Video from "../components/Video/Video";
 import updateVideos from "../actions/searchedVideosActions";
+
+var scrollToTop = function() {};
 
 const VideoContainer = ({ searchedVideos, updateVideos, match, history }) => {
     const { searchedVideo } = match.params;
     const { videoID } = match.params;
+
     useEffect(function() {
         updateVideos(searchedVideo, videoID);
     }, []);
 
-    // Update URL params
+    // for user search
     useEffect(
         function() {
+            scrollToTop();
             var pathname = searchedVideos.currentVideo.id
                 ? `/${searchedVideos.query}/${searchedVideos.currentVideo.id.videoId}`
                 : `/${searchedVideos.query}`;
@@ -20,6 +25,20 @@ const VideoContainer = ({ searchedVideos, updateVideos, match, history }) => {
             history.push({ pathname });
         },
         [searchedVideos.currentVideo.id]
+    );
+
+    // Update URL params
+    useEffect(
+        function() {
+            if (
+                searchedVideos.currentVideo.id &&
+                searchedVideos.currentVideo.id.videoId !== videoID
+            ) {
+                scrollToTop();
+                updateVideos(searchedVideo, videoID);
+            }
+        },
+        [videoID]
     );
 
     return <Video searchedVideos={searchedVideos} />;
